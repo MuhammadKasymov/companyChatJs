273aaae118063870f3153d32bdb2e5b04ba68270
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-03-14 21:49:39 +0500

    added header to choose chat

--- src/components/ChooseChat/ChooseChat.js
+++ src/components/ChooseChat/ChooseChat.js
@@ -4,14 +4,16 @@ import Frame from "../Frame/Frame";
 import ChatLine from "../ChatLine/ChatLine";
 import { useLocation } from "react-router-dom";
 import { getChatIdByUrl } from "../../common/composeString";
+import HorizontalRule from "../../components/HorizontalRule/HorizontalRule";
 
 const ChooseChat = ({ chatsData }) => {
   const history = useLocation();
   const currentLocation = history.pathname;
   const currentChat = getChatIdByUrl(currentLocation);
-  console.log(chatsData);
   return (
     <Frame style={styles.container}>
+      <h1>Список чатов</h1>
+      <HorizontalRule />
       {chatsData.map((chatData) => {
         const isChoosed = currentChat === chatData.id;
         return (
