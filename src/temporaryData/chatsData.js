efe68c94fa32130451a43601df37ed8c203a8f85
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-03-15 02:43:06 +0500

    fixed temporary chats data

--- src/temporaryData/chatsData.js
+++ src/temporaryData/chatsData.js
@@ -1,76 +1,85 @@
 export const data = {
-  "chats": [
+  chats: [
     {
-      "id": 0,
-      "name": "Рабочий чат",
-      "chatHistorie": [
+      id: 0,
+      name: "Рабочий чат",
+      chatHistorie: [
         {
-          "id": 1,
-          "date": 1628654260,
-          "txt": "Привет",
-          "user": "Алексей"
+          id: 1,
+          date: 1628654260,
+          txt: "Привет",
+          userId: 1,
+          user: "Алексей",
         },
         {
-          "id": 2,
-          "date": 1628654430,
-          "txt": "Добрый вечер",
-          "user": "Сергей"
+          id: 2,
+          date: 1628654430,
+          txt: "Добрый вечер",
+          userId: 2,
+          user: "Сергей",
         },
         {
-          "id": 3,
-          "date": 1628654579,
-          "txt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          "user": "Алексей"
-        }
-      ]
+          id: 3,
+          date: 1628654579,
+          userId: 3,
+          txt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          user: "Алексей",
+        },
+      ],
     },
     {
-      "id": 1,
-      "name": "Рандомный чат",
-      "chatHistorie": [
+      id: 1,
+      name: "Рандомный чат",
+      chatHistorie: [
         {
-          "id": 1,
-          "date": 1628654260,
-          "txt": "Привет",
-          "user": "Алексей"
+          id: 1,
+          date: 1628654260,
+          txt: "Привет",
+          userId: 1,
+          user: "Алексей",
         },
         {
-          "id": 2,
-          "date": 1628654430,
-          "txt": "Добрый вечер",
-          "user": "Сергей"
+          id: 2,
+          date: 1628654430,
+          txt: "Добрый вечер",
+          userId: 2,
+          user: "Сергей",
         },
         {
-          "id": 3,
-          "date": 1628654579,
-          "txt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          "user": "Алексей"
-        }
-      ]
+          id: 3,
+          date: 1628654579,
+          userId: 3,
+          txt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          user: "Алексей",
+        },
+      ],
     },
     {
-      "id": 2,
-      "name": "Интересный чат",
-      "chatHistorie": [
+      id: 2,
+      name: "Интересный чат",
+      chatHistorie: [
         {
-          "id": 1,
-          "date": 1628654260,
-          "txt": "Привет",
-          "user": "Алексей"
+          id: 1,
+          date: 1628654260,
+          txt: "Привет",
+          userId: 1,
+          user: "Алексей",
         },
         {
-          "id": 2,
-          "date": 1628654430,
-          "txt": "Добрый вечер",
-          "user": "Сергей"
+          id: 2,
+          date: 1628654430,
+          txt: "Добрый вечер",
+          userId: 2,
+          user: "Сергей",
         },
         {
-          "id": 3,
-          "date": 1628654579,
-          "txt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          "user": "Алексей"
-        }
-      ]
-    }
-  ]
-}
+          id: 3,
+          date: 1628654579,
+          userId: 3,
+          txt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          user: "Алексей",
+        },
+      ],
+    },
+  ],
+};
