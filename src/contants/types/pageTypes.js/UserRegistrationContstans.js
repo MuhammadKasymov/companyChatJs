e3a875aab3b010f7b6bb811c465c9187f0d1b6a2
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-01 13:20:20 +0500

    new contstants for registration page

---
+++ src/contants/types/pageTypes.js/UserRegistrationContstans.js
@@ -0,0 +1,9 @@
+export const inputTypes = {
+  firstName: 'firstName',
+  secontName: 'secondName',
+  lastName: 'lastName',
+  birthday: 'birthday',
+  login: 'login',
+  password: 'password',
+  email: 'email',
+}
