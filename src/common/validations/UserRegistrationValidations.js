98fa75a004bc48a92db56c0054a792b9f5528810
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 22:17:55 +0500

    switch case with input type exceptions

--- src/common/validations/UserRegistrationValidations.js
+++ src/common/validations/UserRegistrationValidations.js
@@ -1,118 +1,66 @@
 import {
-  mailReg,
-  latinAndNumbersReg,
-  latinAndCyrrilReg,
-  dateReg,
-  lettersAndNotOnlyReg,
-  numberndNotOnlyReg,
-  latinCyrrilNumbReg,
-} from "../constans/validations/regularExpressions.js";
+  validDate,
+  validEMail,
+  validLogin,
+  validName,
+  validPassword,
+  validRepeatedPassword,
+} from "./validateInputs";
+import { inputTypes } from "../../contants/types/pageTypes/UserRegistrationContstans";
 import {
-  MIN_YEAR,
-  MAX_MAIL_LENGTH,
-  MAX_LOGIN_LENGTH,
-  MIN_LOGIN_LENGTH,
-  MAX_NAME_LENGTH,
-  MIN_NAME_LENGTH,
-  MIN_PASSWORD_LENGTH,
-  MIN_LENGTH_BIRTHPLACE,
-} from "../constans/validations/userInfo.js";
-import { CURRENT_YEAR, CURRENT_MONTH, CURRENT_DAY } from "../constans/date.js";
-import { getMonthsLengthList } from "../common/date.js";
+  checkLogin,
+  checkEmail,
+} from "../../controllers/registrationController";
+import {
+  EXIST_EMAIL,
+  EXIST_LOGIN,
+} from "../../contants/types/exceptionTypes/registrationExceptionTypes";
 
-export const isString = (variable) => {
-  if (typeof variable !== "string") {
-    return false;
-  }
-  return true;
-};
-
-export const isValidEMail = (email) => {
-  if (!isString(email)) {
-    return false;
-  }
-  const emailLength = email.length;
-  return mailReg.test(email) && emailLength < MAX_MAIL_LENGTH;
-};
-
-export const isValidLogin = (login) => {
-  if (!isString(login)) {
-    return false;
-  }
-  const logLength = login.length;
-  const isGoodLength =
-    logLength <= MAX_LOGIN_LENGTH && logLength >= MIN_LOGIN_LENGTH;
-  return latinAndNumbersReg.test(login) && isGoodLength;
-};
-
-export const isValidName = (name) => {
-  if (!isString(name)) {
-    return false;
-  }
-  const logLength = name.length;
-  const isGoodLength =
-    logLength <= MAX_NAME_LENGTH && logLength >= MIN_NAME_LENGTH;
-  return latinAndCyrrilReg.test(name) && isGoodLength;
-};
-
-export const isValidDate = (date) => {
-  let isValid = false; // result
-  if (!isString(date)) {
-    return false;
-  }
-  if (dateReg.test(date)) {
-    /** Decomposing parametr date */
-    const parts = date.split(".");
-    const day = parseInt(parts[0], 10);
-    const month = parseInt(parts[1], 10);
-    const year = parseInt(parts[2], 10);
-
-    /** Getting list of amount of days in choosed year */
-    const monthLengthList = getMonthsLengthList(year);
-
-    /** comparing with current date */
-    const isLastYear = CURRENT_YEAR === year;
-    const isCurrentMonth = CURRENT_MONTH === month;
-
-    /** Final validations with getted data*/
-    const isGoodYear = year > MIN_YEAR && year <= CURRENT_YEAR;
-    const isGoodMaxMonth = isLastYear ? month <= CURRENT_MONTH : month <= 12;
-    const isGoodMonth = month > 0 && isGoodMaxMonth;
-    const isGoodMaxDay = isCurrentMonth
-      ? day <= CURRENT_DAY
-      : day <= monthLengthList[month - 1];
-    const isGoodDay = day > 0 && isGoodMaxDay;
-    if (isGoodMonth && isGoodYear && isGoodDay) {
-      isValid = true;
-    }
+export async function userRegistrationValidations(
+  inputType,
+  value,
+  extraValue
+) {
+  let result = null;
+  let isExist = false;
+  switch (inputType) {
+    case inputTypes.login:
+      isExist = await checkLogin(value);
+      if (!isExist) {
+        result = validLogin(value);
+      } else {
+        result = EXIST_LOGIN;
+      }
+      break;
+    case inputTypes.email:
+      isExist = await checkEmail(value);
+      if (!isExist) {
+        result = validEMail(value);
+      } else {
+        result = EXIST_EMAIL;
+      }
+      break;
+    case inputTypes.firstName:
+      result = validName(value);
+      break;
+    case inputTypes.secontName:
+      result = validName(value);
+      break;
+    case inputTypes.lastName:
+      result = validName(value);
+      break;
+    case inputTypes.birthday:
+      result = validDate(value);
+      break;
+    case inputTypes.password:
+      result = validPassword(value);
+      break;
+    case inputTypes.repeatedPassword:
+      result = validRepeatedPassword(value, extraValue);
+      break;
+    default:
+      return null;
   }
 
-  return isValid;
-};
-
-export const isValidPassword = (password) => {
-  let isValid = false; // result
-
-  if (isString(password)) {
-    const passwordLength = password.length;
-    const isGoodLength = passwordLength > MIN_PASSWORD_LENGTH;
-    const isIncludeNumbers = numberndNotOnlyReg.test(password);
-    const isIncludeLetters = lettersAndNotOnlyReg.test(password);
-
-    if (isGoodLength && isIncludeLetters && isIncludeNumbers) {
-      isValid = true;
-    }
-  }
-
-  return isValid;
-};
-
-export const isValidBirthplace = (birthPlace) => {
-  if (!isString(birthPlace)) {
-    return false;
-  }
-  const birthPlaceLength = birthPlace.length;
-  const isGoodLength = birthPlaceLength > MIN_LENGTH_BIRTHPLACE;
-
-  return latinCyrrilNumbReg.test(birthPlace) && isGoodLength;
-};
+  return result;
+}
