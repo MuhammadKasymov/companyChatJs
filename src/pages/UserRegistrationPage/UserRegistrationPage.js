03f7e7b3e8a02951e73cfb78bc381a48903b0cb2
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 12:05:58 +0500

    add states and props on Input Components

--- src/pages/UserRegistrationPage/UserRegistrationPage.js
+++ src/pages/UserRegistrationPage/UserRegistrationPage.js
@@ -3,8 +3,20 @@ import Frame from "../../components/Frame/Frame";
 import InputWithHeader from "../../components/InputWithHeader/InputWithHeader";
 import ComponenHeader from "../../components/ComponentHeader/ComponentHeader";
 import styles from "./UserRegistrationPage.module.scss";
+import { inputTypes } from "../../contants/types/pageTypes.js/UserRegistrationContstans";
 
 class UserRegistrationPage extends Component {
+  state = {
+    firstName: "",
+    secontName: "",
+    lastName: "",
+    birthday: "",
+    login: "",
+    password: "",
+    repeatedPassword: "",
+    email: "",
+  };
+
   componentDidMount() {
     console.log("it's started ");
   }
@@ -24,16 +36,40 @@ class UserRegistrationPage extends Component {
       <Frame style={styles.container}>
         <ComponenHeader header="Регистрация" />
         <div className={styles.inputColumn}>
-          <InputWithHeader header={"Логин"} />
-          <InputWithHeader header={"Имя"} />
-          <InputWithHeader header={"Отчество"} />
-          <InputWithHeader header={"Пароль"} />
+          <InputWithHeader
+            onInput={this.typedOnInput(inputTypes.login)}
+            headerText={"Логин"}
+          />
+          <InputWithHeader
+            onInput={this.typedOnInput(inputTypes.firstName)}
+            headerText={"Имя"}
+          />
+          <InputWithHeader
+            onInput={this.typedOnInput(inputTypes.lastName)}
+            headerText={"Отчество"}
+          />
+          <InputWithHeader
+            onInput={this.typedOnInput(inputTypes.password)}
+            headerText={"Пароль"}
+          />
         </div>
         <div className={styles.inputColumn}>
-          <InputWithHeader header={"Email"} />
-          <InputWithHeader header={"Фамилия"} />
-          <InputWithHeader header={"Дата рождения"} />
-          <InputWithHeader header={"Повторитие пароль"} />
+          <InputWithHeader
+            onInput={this.typedOnInput(inputTypes.email)}
+            headerText={"Email"}
+          />
+          <InputWithHeader
+            onInput={this.typedOnInput(inputTypes.secontName)}
+            headerText={"Фамилия"}
+          />
+          <InputWithHeader
+            onInput={this.typedOnInput(inputTypes.birthday)}
+            headerText={"Дата рождения"}
+          />
+          <InputWithHeader
+            onInput={this.typedOnInput(inputTypes.repeatedPassword)}
+            headerText={"Повторитие пароль"}
+          />
         </div>
         <div className={styles.btnsContainer}>
           <button className={styles.btn}>&larr; Авторизоваться</button>
