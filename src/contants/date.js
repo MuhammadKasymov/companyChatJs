8c3e3bebc24d3e94df41af3e5949efc51a7959b0
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 21:12:43 +0500

    decomposed current date

---
+++ src/contants/date.js
@@ -0,0 +1,4 @@
+const currentDate = new Date();
+export const CURRENT_YEAR = currentDate.getFullYear();
+export const CURRENT_MONTH = currentDate.getMonth() + 1;
+export const CURRENT_DAY = currentDate.getDate();
