8b79e2c16d53841da49c8d61f913911e4044875c
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 21:13:25 +0500

    new server url

---
+++ src/contants/server.js
@@ -0,0 +1,4 @@
+export const URL_SERVER = "http://localhost:8081";
+export const CHECK_ROUTE_USER_EMAIL = "/api/check/user/email";
+export const CHECK_ROUTE_USER_LOGIN = "/api/check/user/login";
+export const CHECK_ROUTE_USER_AUTH = "/api/check/user/auth";
