5843cfeb682e1e795fd8bf624d2981d34022a2f1
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 21:12:04 +0500

    new functioon getMonthsLengthList

--- src/common/time.js
+++ src/common/time.js
@@ -32,3 +32,11 @@ export const getFormatedTime = (format, milliseconds) => {
       return "";
   }
 };
+
+/** Get days in month by year */
+export const getMonthsLengthList = (year) => {
+  let febrary = 28;
+  if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) febrary = 29;
+
+  return [31, febrary, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+};
