96c7530ae797e987312a18198ff0a83e5a0d91e1
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 21:13:06 +0500

    new regular expressions

---
+++ src/contants/regularExpressions.js
@@ -0,0 +1,7 @@
+export const mailReg = /\S+@\S+\.\S+/;
+export const latinAndNumbersReg = /^[a-zA-Z0-9]+$/;
+export const latinAndCyrrilReg = /^[a-zA-Zа-яА-ЯЁё]+$/;
+export const dateReg = /^\d{1,2}\.\d{1,2}\.\d{4}$/;
+export const numberndNotOnlyReg = /[0-9]/;
+export const lettersAndNotOnlyReg = /[a-zA-Zа-яА-яЁё]/;
+export const latinCyrrilNumbReg = /^[a-zA-Zа-яА-ЯЁё0-9]+$/;
