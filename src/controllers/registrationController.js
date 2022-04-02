71916c7e64d564058dbd856e106ce8b39c84d4c6
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 22:17:14 +0500

    new request check login

--- src/controllers/registrationController.js
+++ src/controllers/registrationController.js
@@ -1,11 +1,34 @@
 import axios from "axios";
-import { URL_SERVER, CHECK_ROUTE_USER_EMAIL } from "../contants/server";
+import {
+  URL_SERVER,
+  CHECK_ROUTE_USER_EMAIL,
+  CHECK_ROUTE_USER_LOGIN,
+} from "../contants/server";
 
 export const checkEmail = async (email) => {
   let isExist = false;
   await axios
     .post(URL_SERVER + CHECK_ROUTE_USER_EMAIL, {
       email: email,
+    })
+    .then((res) => {
+      const result = res.data;
+      console.log(result);
+      if (res.data) {
+        isExist = result.isExist;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return isExist;
+};
+
+export const checkLogin = async (login) => {
+  let isExist = false;
+  await axios
+    .post(URL_SERVER + CHECK_ROUTE_USER_LOGIN, {
+      login: login,
     })
     .then((res) => {
       const result = res.data;
