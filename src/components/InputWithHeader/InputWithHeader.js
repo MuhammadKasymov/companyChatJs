d478a3453e41a328f4d7fe4af3dfabe17b9d5ca4
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 12:04:55 +0500

    decomposed props and change some postions

--- src/components/InputWithHeader/InputWithHeader.js
+++ src/components/InputWithHeader/InputWithHeader.js
@@ -2,11 +2,21 @@ import React from "react";
 import styles from "./InputWithHeader.module.scss";
 import InputText from "../InputText/InputText";
 
-const InputWithHeader = (props) => {
+const InputWithHeader = ({
+  style,
+  headerText,
+  placeholder,
+  onInput,
+  errorText,
+}) => {
   return (
-    <div className={`${styles.container} ${props.style || ""}`}>
-      <h2 className={styles.headerTxt}>{props.header}</h2>
-      <InputText onInput={props.onInput} placeholder={props.placeholder || ""} />
+    <div className={`${styles.container} ${style || ""}`}>
+      <h2 className={styles.headerTxt}>{headerText}</h2>
+      <InputText
+        errorText={errorText}
+        onInput={onInput}
+        placeholder={placeholder}
+      />
     </div>
   );
 };
