d14129b15ac1676a464efdc5113b36d1a9491fbb
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 21:14:03 +0500

    registr exceptions types

---
+++ src/contants/types/exceptionTypes/registrationExceptionTypes.js
@@ -0,0 +1,11 @@
+export const ONLY_LAT_KYR_LETTERS = "Разрешена только латиница и кириллица";
+export const MIN_TWO_SYMBOLS = "Необходимы минимум 2 символа";
+export const EXIST_LOGIN = "Введенный логин уже зарегистрирован";
+export const EXIST_EMAIL = "Введенный email уже зарегистрирован";
+export const SHORT_LENGTH_PASSWORD =
+  "Пароль должен содержать минимум 6 символов";
+export const BAD_INCLUDE_PASSWORD =
+  "В пароль должен содержать Латиницу/Кириллицу и арабские цифры";
+export const REPEAT_PASSWORD = "Повторите пароль";
+export const BAD_REPEAT_PASSWORD = "Пароли не совпадают";
+export const BAD_FORMAT_DATE = "Дата должна быть формата дд.мм.гггг";
