5edc17db0c30c3c375f5b6f2dd4e1189c6fc7fff
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 21:31:00 +0500

    new exception ONLY_NUMB_LAT_LETTERS

---
+++ src/contants/validations/userRegistration.js
@@ -0,0 +1,8 @@
+export const MIN_YEAR = 1920;
+export const MAX_MAIL_LENGTH = 319;
+export const MAX_LOGIN_LENGTH = 18;
+export const MIN_LOGIN_LENGTH = 2;
+export const MAX_NAME_LENGTH = 24;
+export const MIN_NAME_LENGTH = 2;
+export const MIN_PASSWORD_LENGTH = 6;
+export const MIN_LENGTH_BIRTHPLACE = 2;
