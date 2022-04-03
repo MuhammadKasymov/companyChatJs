10a9c16e3357bf6c0d43ac091aa55f090cd5e2ba
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-03 18:06:23 +0500

    initialState of err inputs for registration page

---
+++ src/contants/initialStates/userRegistrationStates.js
@@ -0,0 +1,10 @@
+export const errorStateInputs = {
+  firstName: null,
+  secontName: null,
+  lastName: null,
+  birthday: null,
+  login: null,
+  password: null,
+  repeatedPassword: null,
+  email: null,
+};
