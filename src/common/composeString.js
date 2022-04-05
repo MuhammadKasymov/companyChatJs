c84082ba12123d305b8397802e908182dec7c7e4
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-05 16:59:17 +0500

    new getOnlyNumbers func

--- src/common/composeString.js
+++ src/common/composeString.js
@@ -1,3 +1,5 @@
+import { numberReg } from "../constants/regularExpressions";
+
 /** Catching string by lenght,
  *  and adding triple dot to end
  *  + Example:
@@ -22,12 +24,24 @@ export const addTripleDot = (str, len) => {
  *  @param {string} url - string;
  *  @returns {number} id of url  */
 export const getChatIdByUrl = (url) => {
-  let chatId = -1; 
+  let chatId = -1;
   const partsOfUlr = url.split("/");
   const lastIndexParts = partsOfUlr.length - 1;
   const lastPartUlr = partsOfUlr[lastIndexParts];
   if (lastPartUlr.match(/^\d+$/)) {
-    chatId = parseInt(lastPartUlr, 10)
-  };
+    chatId = parseInt(lastPartUlr, 10);
+  }
   return chatId;
+};
+
+/** Remove all characters in a
+ * string that are not numbers */
+export const getOnlyNumbers = (str) => {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (numberReg.test(str[i])) {
+      result += str[i];
+    }
+  }
+  return result;
 };
