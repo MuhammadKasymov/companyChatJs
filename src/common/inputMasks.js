65380ca27513e09cd134e09125757deeb1b37132
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-05 16:59:58 +0500

    inputMask date func

---
+++ src/common/inputMasks.js
@@ -0,0 +1,44 @@
+import { getOnlyNumbers } from "./composeString";
+import { inputTypes } from "../constants/types/inputTypes";
+
+export const getMaskedDate = (date) => {
+  let result = "";
+
+  let clearedDate = getOnlyNumbers(date);
+  const dateLength = clearedDate.length;
+
+  if (dateLength <= 2) {
+    result = clearedDate;
+  } else if (dateLength > 2 && dateLength <= 4) {
+    const days = clearedDate.slice(0, 2);
+    const month = clearedDate.slice(2, 4);
+    result = `${days}.${month}`;
+  } else {
+    const days = clearedDate.slice(0, 2);
+    const month = clearedDate.slice(2, 4);
+    const years = clearedDate.slice(4, 8);
+    result = `${days}.${month}.${years}`;
+  }
+
+  return result;
+};
+
+//Todo: fill func
+export const getMasketPhone = (number) => {
+  return number;
+};
+
+export const getMaskedInput = (value, inputType) => {
+  let result = "";
+  switch (inputType) {
+    case inputTypes.DATE:
+      result = getMaskedDate(value);
+      break;
+    case inputTypes.PHONE:
+      result = getMasketPhone(value);
+      break;
+    default:
+      result = value;
+  }
+  return result;
+};
