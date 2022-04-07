bffe672c5358446816123bd7b485ad584368a992
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-07 19:44:21 +0500

    fix valid header component

--- src/components/Frame/Frame.js
+++ src/components/Frame/Frame.js
@@ -1,13 +1,12 @@
 import React from "react";
 import styles from "./Frame.module.scss";
 import ComponentHeader from "../ComponentHeader/ComponentHeader";
-import { isEmptyString } from "../../common/validations/stringValidations";
 
 const Frame = ({ children, style, headerText }) => {
   const additionalStyle = style || "";
   return (
     <div className={`${styles.container} ${additionalStyle}`}>
-      {!isEmptyString(headerText) && <ComponentHeader header={headerText} />}
+      {headerText && <ComponentHeader header={headerText} />}
       {children}
     </div>
   );
