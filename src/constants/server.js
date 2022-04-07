6797db09288bcd66ef9e24628fcf421f7dc691d0
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-07 19:44:39 +0500

    fix auth path

--- src/constants/server.js
+++ src/constants/server.js
@@ -2,4 +2,4 @@ export const URL_SERVER = "http://localhost:8081";
 export const ROUTE_USER_NEW = "/api/user/new";
 export const CHECK_ROUTE_USER_EMAIL = "/api/check/user/email";
 export const CHECK_ROUTE_USER_LOGIN = "/api/check/user/login";
-export const CHECK_ROUTE_USER_AUTH = "/api/check/user/auth";
+export const ROUTE_AUTH = "/api/auth";
