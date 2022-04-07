32f9e381b83c6e3013e4a2ab0435911240e425ac
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-07 19:43:40 +0500

    fix chat route, add id

--- src/constants/routePath.js
+++ src/constants/routePath.js
@@ -1,4 +1,4 @@
 export const baseUrl = "/";
-export const chatRoute = "/chats/";
+export const chatRoute = "/chats/:chatId";
 export const authRoute = "/auth";
 export const registrationRoute = "/registration";
