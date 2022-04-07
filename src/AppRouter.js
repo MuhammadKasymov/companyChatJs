d43e16dfbb81cf9c9392d1743907c19b1355f49b
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-07 19:40:09 +0500

    add auth path

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,14 +1,16 @@
 import React from "react";
 import HomePage from "./pages/HomePage/HomePage";
+import AuthorizationPage from "./pages/AuthorizationPage/AuthorizationPage";
 import UserRegistrationPage from "./pages/UserRegistrationPage/UserRegistrationPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const AppRouter = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<UserRegistrationPage />} />
+      <Route path="/" element={<AuthorizationPage />} />
       <Route path="/chat/:chatId" element={<HomePage />} />
-      <Route path="/registration" element={<HomePage />} />
+      <Route path="/registration" element={<UserRegistrationPage />} />
+      <Route path="/auth" element={<AuthorizationPage />} />
     </Routes>
   </BrowserRouter>
 );
