fdaa1012795164445cd577d9a65c08d3c8f5a45c
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-18 23:30:55 +0500

    add ws conn

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,4 +1,5 @@
 export const URL_SERVER = "http://localhost:8081";
+export const WS_SERVER = "ws://localhost:8081";
 export const ROUTE_USER_NEW = "/api/user/new";
 export const CHECK_ROUTE_USER_EMAIL = "/api/check/user/email";
 export const CHECK_ROUTE_USER_LOGIN = "/api/check/user/login";
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,22 +3,47 @@ import styles from "./HomePage.module.scss";
 import { getChats, getLastMessages } from "../../controllers/chatController";
 import ChooseChat from "../../components/ChooseChat/ChooseChat";
 import Chat from "../../components/Chat/Chat";
+import { WS_SERVER } from "../../constants/server";
 
 class HomePage extends React.Component {
   state = {
     NeadLoad: true,
+    isConnWS: false,
     lastMessagesData: [{ id: -1, lastMessage: "", date: 0 }],
     chatsData: [{}],
   };
 
   async componentDidMount() {
     await this.uploadData();
+    await this.connectWebsocket();
   }
+
+  async connectWebsocket(userId) {
+    const conn = new WebSocket(`${WS_SERVER}/userData?userId=${userId}`);
+    conn.onopen = () => {
+      this.setState({ isConnWS: true });
+    };
+
+    conn.onclose = () => {
+      this.setState({ isConnWS: false });
+    };
+
+    conn.onmessage = (msg) => {};
+  }
+
+  dispathMessageMSG = (msg) => {
+    switch (msg.type) {
+      case "newMessage":
+        break;
+      default:
+        console.log("Bad ws message type");
+        break;
+    }
+  };
 
   uploadData = async () => {
     const lastMessages = await getLastMessages();
     const chats = await getChats();
-
     this.setState({
       NeadLoad: false,
       lastMessagesData: lastMessages,
