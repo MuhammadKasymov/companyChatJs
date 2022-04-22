70e780e016727ffcb5f43394ed39bbc65c1f06d2
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-22 21:20:34 +0500

    remove unusing params on chatLine

--- src/components/ChooseChat/ChooseChat.js
+++ src/components/ChooseChat/ChooseChat.js
@@ -2,30 +2,22 @@ import React from "react";
 import styles from "./ChooseChat.module.scss";
 import Frame from "../Frame/Frame";
 import ChatLine from "../ChatLine/ChatLine";
-import { useLocation } from "react-router-dom";
-import { getChatIdByUrl } from "../../common/composeString";
 import HorizontalRule from "../../components/HorizontalRule/HorizontalRule";
 
 const ChooseChat = ({ chatsData }) => {
-  const history = useLocation();
-  const currentLocation = history.pathname;
-  const currentChat = getChatIdByUrl(currentLocation);
   return (
     <Frame style={styles.container}>
       <h1>Список чатов</h1>
       <HorizontalRule />
-      {chatsData.map((chatData) => {
-        const isChoosed = currentChat === chatData.id;
-        return (
-          <ChatLine
-            key={chatData.id}
-            isChoosed={isChoosed}
-            name={chatData.title}
-            user={chatData.user}
-            lastMessage={chatData.lastMessage}
-          />
-        );
-      })}
+      {chatsData.map((chatData) => (
+        <ChatLine
+          key={chatData.id}
+          chatId={chatData.id}
+          name={chatData.title}
+          lastMessage={chatData.lastMessage}
+        />
+      ))}
+
     </Frame>
   );
 };
