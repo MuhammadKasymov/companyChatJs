57c03214614dac1f5177304de3bce126775fd8a6
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-26 14:38:54 +0500

    deleted old project

--- old/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
-import {Provider} from 'react-redux';
-
-render (
-    <App />,
-  document.getElementById('root')
-);
