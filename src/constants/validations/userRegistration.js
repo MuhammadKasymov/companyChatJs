dea2e891aaf9f06916c5be6c1b4bc8379cb2f33f
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-07 11:32:15 +0500

    change max login length from 18 to 12

--- src/constants/validations/userRegistration.js
+++ src/constants/validations/userRegistration.js
@@ -1,6 +1,6 @@
 export const MIN_YEAR = 1920;
 export const MAX_MAIL_LENGTH = 319;
-export const MAX_LOGIN_LENGTH = 18;
+export const MAX_LOGIN_LENGTH = 12;
 export const MIN_LOGIN_LENGTH = 2;
 export const MAX_NAME_LENGTH = 24;
 export const MIN_NAME_LENGTH = 2;
