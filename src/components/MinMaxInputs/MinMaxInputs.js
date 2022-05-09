cf27741833d4f114ee8bdf3a1edf8a502bed841f
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-09 16:35:02 +0500

    fix some params of MinMaxInputs

--- src/components/MinMaxInputs/MinMaxInputs.js
+++ src/components/MinMaxInputs/MinMaxInputs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { inputTypes } from "../../constants/types/inputTypes";
 import InputText from "../InputText/InputText";
 import styles from "./MinMaxInputs.module.scss";
 
@@ -6,11 +7,21 @@ const MinMaxInputs = ({ onMinInput, onMaxInput, style }) => {
   return (
     <div className={`${styles.container} ${style || ""} `}>
       <div className={styles.inputContainer}>
-        <InputText onInput={onMinInput} placeholder={"Мин"} />
+        <InputText
+          inputStyle={styles.input}
+          inputType={inputTypes.NUMBERS}
+          onInput={onMinInput}
+          placeholder={"Мин"}
+        />
       </div>
       <p className={styles.sign}>-</p>
       <div className={styles.inputContainer}>
-        <InputText onInput={onMaxInput} placeholder={"Макс"} />
+        <InputText
+          inputStyle={styles.input}
+          inputType={inputTypes.NUMBERS}
+          onInput={onMaxInput}
+          placeholder={"Макс"}
+        />
       </div>
     </div>
   );
