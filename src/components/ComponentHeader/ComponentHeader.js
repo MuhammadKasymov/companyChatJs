80fcbf0106aea2e423504d01316e030045101548
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-10 18:45:18 +0500

    provide style to props of ComponentHeader

--- src/components/ComponentHeader/ComponentHeader.js
+++ src/components/ComponentHeader/ComponentHeader.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./ComponentHeader.module.scss";
 
-const ComponentHeader = ({ header }) => {
+const ComponentHeader = ({ header, style }) => {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${style || ""}`}>
       <h1 className={styles.headerText}>{header}</h1>
     </div>
   );
