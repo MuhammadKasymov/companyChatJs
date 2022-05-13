b8bc022b71a09380c551ad014044a037eed53aec
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-13 15:31:05 +0500

    add notif counter to UserNameRow

--- src/components/NavBar/components/UserNameRow/UserNameRow.js
+++ src/components/NavBar/components/UserNameRow/UserNameRow.js
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./UserNameRow.module.scss";
 
-const UserNameRow = ({ selfData, isOpen }) => {
+const UserNameRow = ({ selfData, isOpen, changeVisibility, notifLength }) => {
+  const getMaxedNotif = () => (notifLength > 99 ? "99+" : notifLength);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.ico}>{selfData.login?.charAt(0)}</div>
+    <div className={styles.container} onClick={changeVisibility}>
+      <div className={styles.ico}>
+        <p className={styles.icoText}>{selfData.login?.charAt(0)} </p>
+        {notifLength > 0 && (
+          <span className={styles.notifLength}>
+            <p>{getMaxedNotif()}</p>{" "}
+          </span>
+        )}
+      </div>
+
       <p className={styles.userNameText}>
         {`${selfData?.firstName} ${selfData?.secondName}`}
       </p>
