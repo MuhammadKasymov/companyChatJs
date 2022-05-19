5f402b2869a6a6891531d7d66b6c59551bfa03ef
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-19 06:38:13 +0500

    fix getCompressedImg, remake to promise

--- src/common/files.js
+++ src/common/files.js
@@ -1,27 +1,29 @@
-export const getCompressedImg = (file, maxWidth) => {
+export const getCompressedImg = async (file, maxWidth) => {
   if (!file) return "Error";
   let result = "";
   const fileType = file.type;
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
 
-  reader.onload = function (event) {
-    const imgElem = document.createElement("img");
-    imgElem.src = event.target.result;
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (event) => {
+      const imgElem = document.createElement("img");
+      imgElem.src = event.target.result;
 
-    imgElem.onload = (ev) => {
-      const canvast = document.createElement("canvas");
-      const MAX_WIDTH = maxWidth || 880;
-      const target = ev.target;
-      const scaleSize = MAX_WIDTH / target.width;
-      canvast.width = MAX_WIDTH;
-      canvast.height = target.height * scaleSize;
-
-      const ctx = canvast.getContext("2d");
-      ctx.drawImage(target, 0, 0, canvast.width, canvast.height);
-      result = ctx.canvas.toDataURL(target, fileType);
+      imgElem.onload = (ev) => {
+        const canvast = document.createElement("canvas");
+        const MAX_WIDTH = maxWidth || 880;
+        const target = ev.target;
+        const scaleSize = MAX_WIDTH / target.width;
+        canvast.width = MAX_WIDTH;
+        canvast.height = target.height * scaleSize;
+        const ctx = canvast.getContext("2d");
+        ctx.drawImage(target, 0, 0, canvast.width, canvast.height);
+        result = ctx.canvas.toDataURL(target, fileType);
+        resolve(result);
+      };
+      imgElem.onerror = (err) => reject(err);
     };
-  };
-
-  return result;
+    reader.onerror = (err) => reject(err);
+  });
 };
