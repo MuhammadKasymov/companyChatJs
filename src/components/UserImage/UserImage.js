b27fa61dcf659a047f30aea39c0f9f91a7be4903
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-23 17:07:11 +0500

    add upload img to UserImage component

--- src/components/UserImage/UserImage.js
+++ src/components/UserImage/UserImage.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useIsMounted } from "../../common/hooks";
 import styles from "./UserImage.module.scss";
+import { getImageById } from "../../controllers/files";
 
-const UserImage = ({ login, srcImage }) => {
+const UserImage = ({ login, imageId }) => {
+  const [image, setImage] = useState(null);
+  const isMounted = useIsMounted();
+
+  const uploadImg = useCallback(async () => {
+    if (imageId != null) {
+      const imgData = await getImageById(imageId);
+      isMounted.current && setImage(imgData?.image);
+    }
+  }, [imageId, isMounted]);
+
+  useEffect(uploadImg, [uploadImg]);
+
   const textIco = login?.charAt(0).toUpperCase();
   return (
     <div className={styles.container}>
-      {srcImage ? (
-        <img className={styles.img} alt={"Аватарка"} src={srcImage} />
+      {image ? (
+        <img className={styles.img} alt={"Аватарка"} src={image} />
       ) : (
         <p className={styles.textImg}>{textIco}</p>
       )}
