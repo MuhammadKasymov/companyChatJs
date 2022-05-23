03a5d4decf2d3291b015f34c514856775a01e9a3
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-23 10:27:26 +0500

    add new component of UserIcon

---
+++ src/components/UserIcon/UserIcon.js
@@ -0,0 +1,63 @@
+import React, { useCallback, useEffect, useState } from "react";
+import styles from "./UserIcon.module.scss";
+import { getImageById } from "../../controllers/files";
+import { useIsMounted } from "../../common/hooks";
+import UserInfoModal from "../UserInfoModal/UserInfoModal";
+
+const UserIcon = ({ userData, setData, showModal }) => {
+  const [isChoosed, setIsChoosed] = useState(false);
+  const [userImg, setUserImage] = useState(null);
+  const [isMouseOver, setIsMouseOver] = useState(true);
+  const isMounted = useIsMounted();
+  let showTimeOut = null;
+  const firstLetter = userData.login?.charAt(0)?.toUpperCase();
+  const updateNotif = useCallback(async () => {
+    if (userData.imageId != null) {
+      const imgData = await getImageById(userData.imageId);
+      isMounted.current && setUserImage(imgData?.image);
+    }
+  }, [isMounted, userData.imageId]);
+
+  useEffect(updateNotif, [updateNotif]);
+
+  const onClickEv = () => {
+    if (setData) {
+      setIsChoosed((prevState) => !prevState);
+      setData(userData, isChoosed);
+    }
+  };
+  const mouseOvered = () => {
+    if (showModal) {
+      showTimeOut && clearTimeout(showTimeOut);
+      showTimeOut = setTimeout(() => setIsMouseOver(true), 400);
+    }
+  };
+  const mouseLeaved = () => {
+    if (showModal) {
+      isMouseOver && setIsMouseOver(false);
+      showTimeOut && clearTimeout(showTimeOut);
+    }
+  };
+
+  return (
+    <button
+      onClick={onClickEv}
+      onMouseEnter={mouseOvered}
+      onMouseLeave={mouseLeaved}
+      className={`${styles.container} ${isChoosed ? styles.border : ""}`}
+    >
+      {userImg === null ? (
+        <div className={styles.img}>{firstLetter}</div>
+      ) : (
+        <img
+          src={userImg}
+          className={`${styles.img} ${isChoosed ? styles.imgBorder : ""}`}
+          alt={"Иконка"}
+        />
+      )}
+      <UserInfoModal isShow={isMouseOver} userData={userData} />
+    </button>
+  );
+};
+
+export default UserIcon;
