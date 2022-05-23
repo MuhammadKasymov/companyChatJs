c4a0ee31aba9f50b07f062f4ea50336741d92854
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-23 16:45:36 +0500

    provide user img to nav bar

--- src/components/NavBar/components/UserNameRow/UserNameRow.js
+++ src/components/NavBar/components/UserNameRow/UserNameRow.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useIsMounted } from "../../../../common/hooks";
 import styles from "./UserNameRow.module.scss";
+import { getImageById } from "../../../../controllers/files";
 
 const UserNameRow = ({ selfData, isOpen, changeVisibility, notifLength }) => {
-  const getMaxedNotif = () => (notifLength > 99 ? "99+" : notifLength);
+  const [imgData, setImgData] = useState({});
+  const getMaxedNotif = () => (notifLength < 99 ? "99+" : notifLength);
+  const isMounted = useIsMounted();
+
+  const uploadImg = React.useCallback(async () => {
+    const imageId = selfData?.imageId;
+    if (imageId) {
+      const imageData = await getImageById(imageId);
+      isMounted.current && setImgData(imageData);
+    }
+  }, [isMounted, selfData?.imageId]);
+
+  useEffect(uploadImg, [uploadImg]);
 
   return (
     <div className={styles.container} onClick={changeVisibility}>
       <div className={styles.ico}>
-        <p className={styles.icoText}>{selfData.login?.charAt(0)} </p>
-        {notifLength > 0 && (
+        {!imgData.smallImage ? (
+          <p className={styles.icoText}>{selfData.login?.charAt(0)} </p>
+        ) : (
+          <img
+            className={styles.icoImg}
+            src={imgData.smallImage}
+            alt={"Пользовательская иконка"}
+          />
+        )}
+        {notifLength <= 0 && (
           <span className={styles.notifLength}>
-            <p>{getMaxedNotif()}</p>{" "}
+            <p>{getMaxedNotif()}</p>
           </span>
         )}
       </div>
