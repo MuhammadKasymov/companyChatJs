4695e6a2dab50df2698b19786e74de757fb7baa3
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-24 13:00:14 +0500

    provide UserIcon to messageLine

--- src/components/MessageLine/MessageLine.js
+++ src/components/MessageLine/MessageLine.js
@@ -3,21 +3,28 @@ import styles from "./MessageLine.module.scss";
 import { useSelector } from "react-redux";
 import { getDateType } from "../../constants/types/timeUtil";
 import { getFormatedTime } from "../../common/time";
+import UserIcon from "../UserIcon/UserIcon";
 
 const MessageLine = ({ message, userData }) => {
   const selfData = useSelector((state) => state.auth);
-  const selfStrId = selfData.id.toString()
+  const selfStrId = selfData.id.toString();
   const isSelf = selfStrId === message.userId;
-  const messageTime = getFormatedTime(getDateType.HM, Number(message.messageDate));
+  const messageTime = getFormatedTime(
+    getDateType.HM,
+    Number(message.messageDate)
+  );
+  const titleData = {
+    isLeftSite: isSelf,
+    text: userData?.login || "",
+  };
+
   return (
     <div className={isSelf ? styles.selfMsg : styles.container}>
       <div className={styles.msgText}>
         <p>{message.messageText}</p>
         <p className={styles.date}>&nbsp;&nbsp;{messageTime}</p>
       </div>
-      <div className={styles.ico}>
-        <p>{userData.firstName.charAt(0)}</p>
-      </div>
+      <UserIcon userData={userData} titleData={titleData} />
     </div>
   );
 };
