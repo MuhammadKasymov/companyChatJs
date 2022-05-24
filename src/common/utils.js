13441ce115b1ec31fbba0c9d26c47f4a2b40347d
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-24 09:30:40 +0500

    add new util of scrollToChild

--- src/common/utils.js
+++ src/common/utils.js
@@ -13,3 +13,21 @@ export const isEmpty = (obj) => {
     return false;
   }
 };
+
+/** If el is not in viewport then scroll into view
+ * @param parent - parent el
+ * @param child - child el
+ */
+export const scrollToChild = (parent, child) => {
+  const elTopScroll = child.offsetTop;
+  const elScrollHeigth = child.scrollHeight;
+  const elYPos = elTopScroll - elScrollHeigth;
+  const parHeigth = parent.clientHeight;
+  const parScrollTop = parent.scrollTop;
+  const parCurrentView = parHeigth + parScrollTop;
+  const isBottomVisible = parCurrentView > elTopScroll;
+  const isTopVisible = parScrollTop < elYPos;
+  const isVisible = isBottomVisible && isTopVisible;
+
+  !isVisible && child.scrollIntoView();
+};
