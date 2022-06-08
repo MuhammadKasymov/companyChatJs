9e8e238a8de816861bbbd974bbc7792fffee1349
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-06-08 15:20:27 +0500

    add getDateDiff & getDelayDateType to time common

--- src/common/time.js
+++ src/common/time.js
@@ -1,4 +1,8 @@
-import { getDateType } from "../constants/types/timeUtil";
+import {
+  getDateType,
+  diffDateType,
+  delayType,
+} from "../constants/types/timeUtil";
 
 /** Get formated time
  * @param {string} format - type of get data
@@ -7,6 +11,7 @@ import { getDateType } from "../constants/types/timeUtil";
  *  + HOURSE_MINUTES -  hours:minute | 24:00
  *  + ONLY_DATE - day.mounth.year | 10.03.2022
  *  + FULL_TIME - day.mounth.year hours:minute | 10.03.2022 12:33'
+ *  +
  *
  * @param {number} milliseconds - optional parameter if enabled,
  * will format the time from this parameter, and if not, it will
@@ -28,9 +33,61 @@ export const getFormatedTime = (format, milliseconds) => {
     case getDateType.FT: // full - time
       const fullDate = onlyDate + " " + onlyHours.slice(0, -3);
       return fullDate;
+    case getDateType.DM: // day - mounth
+      return onlyDate.slice(0, 5);
     default:
       return "";
   }
+};
+
+/** Get difference of date
+ * todo: complete it...
+ */
+export const getDateDiff = (resType, fDate, lDate) => {
+  let result = "";
+  let t1, t2;
+  let sDate = lDate;
+  if (!lDate) {
+    sDate = new Date();
+  }
+  switch (resType) {
+    case diffDateType.IN_DAYS:
+      t2 = sDate.getTime();
+      t1 = fDate.getTime();
+      result = Math.floor((t2 - t1) / (24 * 3600 * 1000));
+      break;
+    case diffDateType.IN_WEEKS:
+      t2 = sDate.getTime();
+      t1 = fDate.getTime();
+      result = parseInt((t2 - t1) / (24 * 3600 * 1000 * 7));
+      break;
+    case diffDateType.IN_MOUNTH:
+      const fDateY = fDate.getFullYear();
+      const sDateY = sDate.getFullYear();
+      const fDateM = fDate.getMonth();
+      const sDateM = sDate.getMonth();
+      result = sDateM + 12 * sDateY - (fDateM + 12 * fDateY);
+      break;
+    case diffDateType.IN_YEARS:
+      result = sDate.getFullYear() - fDate.getFullYear();
+      break;
+    default:
+      break;
+  }
+  return result;
+};
+
+/** Get Full millisecond in Date
+ * @param {number} date - date, can be empty
+ */
+export const getDateInMilliseconds = (date) => {
+  let result = 0;
+  if (date) {
+    result = date.getTime();
+  } else {
+    result = new Date().getTime();
+  }
+  return result;
 };
 
 /** Get days in month by year */
@@ -39,4 +96,51 @@ export const getMonthsLengthList = (year) => {
   if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) febrary = 29;
 
   return [31, febrary, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+};
+
+/** Get delays text from milliseconds
+ *  @param {number} milliseconds get diff from this time
+ *  @returns {delayType}
+ */
+export const getDelayDateType = (milliseconds) => {
+  const date = milliseconds ? new Date(milliseconds) : new Date();
+  const diffYear = getDateDiff(diffDateType.IN_YEARS, date);
+  const diffDays = getDateDiff(diffDateType.IN_DAYS, date);
+  let type = delayType.MOUNTH_DATE;
+
+  if (diffYear > 0) {
+    type = delayType.FULL_DATE;
+  } else if (diffDays === 1) {
+    type = delayType.YESTERDAY;
+  } else if (diffDays === 0) {
+    type = delayType.TODAY;
+  }
+  return type;
+};
+
+/** get text of delay by type
+ * @param {milliseconds} number
+ * @param {getDateType} type
+ * @returns {string}
+ */
+export const getDateDelayText = (milliseconds, type) => {
+  let result = "";
+  switch (type) {
+    case delayType.FULL_DATE:
+      result = getFormatedTime(getDateType.FT, milliseconds);
+      break;
+    case delayType.MOUNTH_DATE:
+      result = getFormatedTime(getDateType.DM, milliseconds);
+      break;
+    case delayType.YESTERDAY:
+      result = "Вчера";
+      break;
+    case delayType.TODAY:
+      result = "Сегодня";
+      break;
+    default:
+      break;
+  }
+
+  return result;
 };
--- src/constants/types/timeUtil.js
+++ src/constants/types/timeUtil.js
@@ -1,5 +1,20 @@
 export const getDateType = {
-  HM: 'HOURSE_MINUTES',
-  OD: 'ONLY_DATE',
-  FT: 'FULL_TIME',
-}
+  HM: "HOURSE_MINUTES",
+  OD: "ONLY_DATE",
+  FT: "FULL_TIME",
+  DM: "DAY_MOUNTH",
+};
+
+export const diffDateType = {
+  IN_DAYS: "IN_DAYS",
+  IN_WEEKS: "IN_WEEKS",
+  IN_MOUNTH: "IN_MOUNTH",
+  IN_YEARS: "IN_YEARS",
+};
+
+export const delayType = {
+  FULL_DATE: "FULL_DATE",
+  MOUNTH_DATE: "MOUNTH_DATE",
+  YESTERDAY: "YESTERDAY",
+  TODAY: "TODAY",
+};
