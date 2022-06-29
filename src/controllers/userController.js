71845fcc01703a7e816835d5a5f7fa50917422bd
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-06-29 15:41:35 +0500

    add new req of setUserData & checkPassword

---
+++ src/controllers/userController.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import {
+  URL_SERVER,
+  CHECK_ROUTE_PASSWORD,
+  ROUTE_USER_UPDATE,
+} from "../constants/server";
+
+export const setUserData = async (userData) => {
+  let isSuccess = false;
+  await axios
+    .post(URL_SERVER + ROUTE_USER_UPDATE, userData)
+    .then((res) => {
+      const result = res.data;
+      if (result) {
+        isSuccess = true;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  return isSuccess;
+};
+
+export const checkPassword = async (login, password) => {
+  const reqBody = {
+    login,
+    password,
+  };
+  console.log(reqBody)
+  let isGood = false;
+  await axios.post(URL_SERVER + CHECK_ROUTE_PASSWORD, reqBody).then((res) => {
+    const result = res.data;
+    isGood = result.isGood;
+    console.log("isGood", isGood);
+  }).catch(err => console.log(err));
+  return isGood;
+};
